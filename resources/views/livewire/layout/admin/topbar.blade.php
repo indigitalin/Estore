89b4d5805d51d7233ee56660c82d348a37d9cf2d
James <rohithr.indigital@gmail.com>
2024-10-14 14:47:39 +0530

    notification

--- resources/views/livewire/layout/admin/topbar.blade.php
+++ resources/views/livewire/layout/admin/topbar.blade.php
@@ -119,62 +119,14 @@ new class extends Component {
 
                     <!-- Dropdown Start -->
                     <div x-show="dropdownOpen"
-                        class="absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80">
+                        class="absolute -right-27 mt-2.5 flex w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80">
                         <div class="px-4.5 py-3">
                             <h5 class="text-sm font-medium text-bodydark2">Notification</h5>
                         </div>
 
                         <ul class="flex h-auto flex-col overflow-y-auto">
-                            <li>
-                                <a class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                                    href="#">
-                                    <p class="text-sm">
-                                        <span class="text-black dark:text-white">Edit your information in a
-                                            swipe</span>
-                                        Sint occaecat cupidatat non proident, sunt in culpa qui
-                                        officia deserunt mollit anim.
-                                    </p>
-
-                                    <p class="text-xs">12 May, 2025</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                                    href="#">
-                                    <p class="text-sm">
-                                        <span class="text-black dark:text-white">It is a long established
-                                            fact</span>
-                                        that a reader will be distracted by the readable.
-                                    </p>
-
-                                    <p class="text-xs">24 Feb, 2025</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                                    href="#">
-                                    <p class="text-sm">
-                                        <span class="text-black dark:text-white">There are many
-                                            variations</span>
-                                        of passages of Lorem Ipsum available, but the majority have
-                                        suffered
-                                    </p>
-
-                                    <p class="text-xs">04 Jan, 2025</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                                    href="#">
-                                    <p class="text-sm">
-                                        <span class="text-black dark:text-white">There are many
-                                            variations</span>
-                                        of passages of Lorem Ipsum available, but the majority have
-                                        suffered
-                                    </p>
-
-                                    <p class="text-xs">01 Dec, 2024</p>
-                                </a>
+                            <li class="px-4.5 py-3 hover:bg-gray-2">
+                                Opps, no notification available
                             </li>
                         </ul>
                     </div>
