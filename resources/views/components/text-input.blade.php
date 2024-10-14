7c45a6e7a1a8a4bdc2bb00c343ee27a37a9c6a63
James <rohithr.indigital@gmail.com>
2024-10-14 18:43:43 +0530

    fix repo

--- resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $attributes['type'] == 'password' ? 'autocomplete=new-password' : 'autocomplete=off' }}  {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 w-full rounded-md shadow-sm']) !!}>
