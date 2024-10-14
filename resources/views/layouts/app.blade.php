3ffd3466985d0c3e1466f04f2f28db64e7a57597
James <rohithr.indigital@gmail.com>
2024-10-14 19:58:16 +0530

    theme fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     @livewire('wire-elements-modal')
