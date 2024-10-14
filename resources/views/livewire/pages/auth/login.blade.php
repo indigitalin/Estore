9c956baaf1214ac15aeef22b3863cb30fb856f11
Shefii-02 <shefii.km@gmail.com>
2024-10-14 14:45:15 +0530

    hhhdgdg

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -29,9 +29,6 @@ new #[Layout('layouts.guest')] class extends Component {
     <!-- ===== Content Area Start ===== -->
     <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
 
-
-        <!-- ===== Main Content Start ===== -->
-        <main>
             <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
 
                 <!-- ====== Forms Section Start -->
@@ -182,8 +179,7 @@ new #[Layout('layouts.guest')] class extends Component {
                 </div>
                 <!-- ====== Forms Section End -->
             </div>
-        </main>
-        <!-- ===== Main Content End ===== -->
+      
     </div>
     <!-- ===== Content Area End ===== -->
 
