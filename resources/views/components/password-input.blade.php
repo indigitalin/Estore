5eed807f5a70ef75677c14eedb7bf2a0b0311e23
James <rohithr.indigital@gmail.com>
2024-10-17 14:34:02 +0530

    password input

--- resources/views/components/password-input.blade.php
+++ resources/views/components/password-input.blade.php
@@ -3,7 +3,7 @@
 <div class="relative" x-data="{ showPassword: false }">
     <input autocomplete="new-password" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
         'class' =>
-            'w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary',
+            'w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-11 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary',
     ]) !!} :type="showPassword ? 'text' : 'password'">
     <span class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer" @click="showPassword = !showPassword">
         <box-icon color="#888" name='show' x-show="!showPassword"></box-icon>
