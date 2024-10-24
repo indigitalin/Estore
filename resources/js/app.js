8159edf453b1d1ad7969ee8383acede454ebf210
Shefii-02 <shefii.km@gmail.com>
2024-10-24 14:52:54 +0530

    on input or change value time error hidden

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,30 @@ document.addEventListener('livewire:init', () => {
 });
 
 Alpine.plugin(Tooltip);
+
+
+document.addEventListener("input", (event) => {
+    const target = event.target;
+
+    if (target.matches("input[type='text'], input[type='number'], input[type='password'], textarea")) {
+        // Remove the error message (ul.text-red-600) if it exists
+        const errorMessage = target.closest('div').querySelector('ul.text-red-600');
+        if (errorMessage) {
+            errorMessage.remove();
+            console.log("Error message removed for input field.");
+        }
+    }
+});
+
+document.addEventListener("change", (event) => {
+    const target = event.target;
+
+    if (target.matches("select, input[type='checkbox'], input[type='radio']")) {
+        console.log("Select, checkbox, or radio button changed:", target.value);
+        const errorMessage = target.closest('div').querySelector('ul.text-red-600');
+        if (errorMessage) {
+            errorMessage.remove();
+            console.log("Error message removed for select, checkbox, or radio.");
+        }
+    }
+});
