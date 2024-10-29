3ff484a7c932d44ceb7259f5c04a2caf74702ab7
James <rohithr.indigital@gmail.com>
2024-10-29 19:13:39 +0530

    form button spinner

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,9 @@
-<button wire:loading.attr="disabled" {{ $attributes->merge(['type' => 'submit', 'class' => 'capitalize flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90']) }}>
-    {{ $slot }} <span wire:loading></span>
+<button wire:loading.attr="disabled"
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'relative capitalize flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90']) }}>
+    <span wire:loading.class="opacity-0">{{ $slot }} </span>
+    <span class="absolute w-full top-0 start-0 h-100 flex items-center">
+        <span wire:loading class="m-auto">
+            <img class="w-11 opacity-80" src="https://raw.githubusercontent.com/n3r4zzurr0/svg-spinners/main/preview/3-dots-fade-white-36.svg" alt="">
+        </span>
+    </span>
 </button>
