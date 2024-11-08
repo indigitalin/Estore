02792e924b9d76f7b09595a0623a44c1769685c1
rrRRohith <rohithr.indigital@gmail.com>
2024-11-08 14:50:57 +0530

    product types migration

---
+++ database/migrations/2024_11_08_091615_create_product_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->string('handle', 150);
+            $table->bigInteger('client_id')->unsigned()->nullable();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete("cascade");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_types');
+    }
+};
