4fa647e1ccc0d1283c971ecc59fd41f2c1526ee3
rrRRohith <rohithr.indigital@gmail.com>
2024-11-12 16:50:40 +0530

    product stores migrations

---
+++ database/migrations/2024_11_12_111626_create_product_stores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_stores', function (Blueprint $table) {
+            $table->bigInteger('store_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->integer('quantity')->nullable();
+            $table->foreign('store_id')->references('id')->on('stores')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->primary(['store_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_stores');
+    }
+};
