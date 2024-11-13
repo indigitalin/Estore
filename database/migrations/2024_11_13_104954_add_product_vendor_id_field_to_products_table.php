9151b304a83674102567936584dca82656d4e11d
rrRRohith <rohithr.indigital@gmail.com>
2024-11-13 16:20:38 +0530

    product vendor migration

---
+++ database/migrations/2024_11_13_104954_add_product_vendor_id_field_to_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_vendor_id')->nullable()->after('category_id');
+            $table->foreign('product_vendor_id')->references('id')->on('product_vendors')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['product_vendor_id']);
+            $table->dropColumn('product_vendor_id');
+        });
+    }
+};
